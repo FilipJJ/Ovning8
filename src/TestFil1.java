5df647d21252d82eb49504515b74fc5696560ec2
Filip Jakobsson <Filip.Jakobsson@yh.nackademin.se>
2020-10-22 15:54:43 +0200

    Initial commit

---
+++ src/TestFil1.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Filip Jakobsson
+ * Date: 2020-10-22
+ * Time: 15:54
+ * Project: Ovning8
+ * Copyright: MIT
+ */
+public class TestFil1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
